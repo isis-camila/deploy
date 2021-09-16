6d44ceb8d90810418010c75474b555113f943f11
isis-camila <83370439+isis-camila@users.noreply.github.com>
2021-09-15 21:32:09 -0300

    subiendo info

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const baseConfigGenerator = require('./webpack.config.base.js');
+const prodConfig = require('./webpack.config.base.js');
+const devConfig = require('./webpack.config.dev.js');
+
+function webpackEnviromentSelector(env) {
+    let config;
+    if (env.production) config = prodConfig;
+    if (env.development) config = devConfig;
+
+    const baseConfig = baseConfigGenerator(env);
+    return merge(baseConfig, config);
+
+}
+
+module.exports = webpackEnviromentSelector;
